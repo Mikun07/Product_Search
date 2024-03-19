b2cb92bdb20a8ef0d1bb7e1df07cc5adc15bca82
Festus-Olaleye <a.festus-olaleye@ffsdgroup.com>
2024-03-19 23:09:15 +0000

    Cypress config

--- src/components/Search/SearchBar.jsx
+++ src/components/Search/SearchBar.jsx
@@ -7,7 +7,7 @@ const SearchBar = ({ handleSearch }) => {
       <div className="bg-slate-200 relative rounded-lg h-10 p-4 shadow-md flex items-center" data-testid="search-bar">
         <AiOutlineSearch className="text-primary cursor-pointer" />
         <input
-          className="bg-transparent ring-gray-600 outline-none text-sm w-48 ml-2 py-2 text-black placeholder:text-gray-800"
+          className="bg-transparent ring-gray-600 outline-none text-sm lg:w-48 w-36 ml-2 py-2 text-black placeholder:text-gray-800"
           type="text"
           placeholder="Search..."
           onChange={(e) => handleSearch(e)}
