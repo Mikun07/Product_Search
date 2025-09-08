b4cd686e956a3aab2a70e48e1a45c596537ae910
Festus-Olaleye Ayomikun <76745046+Mikun07@users.noreply.github.com>
2025-09-08 09:00:00 +0200

    chore: swap React dependencies for Vue 3, Pinia, and vue-toastification

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,19 +3,14 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    'plugin:vue/vue3-recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  ignorePatterns: ['dist', 'cypress', 'cypress.config.js'],
   rules: {
-    'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'vue/multi-word-component-names': 'off',
   },
 }
